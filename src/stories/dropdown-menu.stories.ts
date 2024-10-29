7652cba3df35c3054e9d46dc0e3290c6552242d7
Arturo García <arturogal95@gmail.com>
2024-10-28 23:45:57 -0600

    Add first version of storybook impl

---
+++ src/stories/dropdown-menu.stories.ts
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import DropdownMenu from './dropdown-menu.vue'
+
+const meta = {
+  title: 'Example/DropdownMenu',
+  component: DropdownMenu,
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {
+    options: [],
+  },
+} satisfies Meta<typeof DropdownMenu>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    options: [
+      'Luis Gerardo García',
+      'Juan Pérez',
+      'Martín Martínez',
+      'Arturo García',
+      'Rodrigo Bentancur',
+      'Cristian Romero',
+      'Pedro Porro',
+    ],
+  },
+}
